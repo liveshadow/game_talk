917103da79cd14924b3d7d82b8509e8b1bbc5419
Erik Eakins <eeakins06@gmail.com>
2015-04-11 16:11:12 -0600

    Game Detail page working...Ugly, but works

--- build/giantbomb/index.js
+++ build/giantbomb/index.js
@@ -1,5 +1,30 @@
 var GameData = {
 
+   details: function(data) {
+        for (var i = 0; i < data.results.genres.length; i++)
+        {
+            console.log(data.results.genres[i].name)
+        }
+        $.get("/giantbomb/details.jade", function(template) {
+            var html = jade.render(template, {
+                data: data.results
+            })
+            $("#list").html(html);
+        })
+   },
+
+   gameDetails: function(game_id) {
+        console.log(game_id)
+        $(document).ready(function(){    
+                $.ajax({
+                    url: "http://api.giantbomb.com/game/"+game_id+"/",
+                    type: "get",
+                    data: {api_key : apikey.apikey_bomb, format: "jsonp", json_callback: "GameData.details"},
+                    dataType: "jsonp"
+                });
+            });
+    },
+
    gamer: function(data) {
         $.get("/giantbomb/list.jade", function(template) {
             var html = jade.render(template, {
