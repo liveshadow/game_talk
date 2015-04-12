6dbb19eda8d80fb9b66cc2c2392b7ded0ce27a91
Erik Eakins <eeakins06@gmail.com>
2015-04-11 21:58:53 -0600

    Compiled all together.. Having issues with actualy reviews being pulled

--- contents/giantbomb/index.js
+++ contents/giantbomb/index.js
@@ -1,16 +1,62 @@
 var GameData = {
 
-   details: function(data, game_name) {
+    details: function(tweetData, revList, genData) {
+        console.log(genData)
+        console.log(revList)
+        console.log(tweetData)
         $.get("/giantbomb/details.jade", function(template) {
             var html = jade.render(template, {
-                data: data.results
+                gen: genData,
+                rev: revList,
+                data: tweetData
             })
             $("#list").html(html);
         })
-   },
+    },
 
-   gameDetails: function(game_id, game_name) {
-        console.log(game_id);
+    tweets: function(revData, genData, game_name) {
+        var revList = []
+        console.log(revData)
+        if (revData.results)
+        {
+            for (var i = 0; i < revData.results.length; i++)
+            {   
+                console.log(revData.results[i].name +" && "+ game_name)
+                if (revDat.results[i].name == game_name)
+                {
+                    revList.push(revDat.results[i])
+                }
+            }
+        }
+        $(document).ready(function(){    
+            var test = $.ajax({
+                url: "http://localhost:8000/tweet_search", // our heroku address will go here until more clever way of referencing self is found ;) ;D
+                type: "get",
+                data: {
+                    q: game_name
+                },
+                dataType: "json", //was jsonp
+                success: function(data) { GameData.details(data,revList, genData) }, 
+                error: function(){ console.log("Error in Ajax Call (oh noes!)"); }
+            });
+        });
+    },
+
+    gameReview: function(genData, game_name) {
+        $(document).ready(function(){    
+            $.ajax({
+                url: "http://api.giantbomb.com/search/?json_callback=?",
+                type: "get",
+                data: {api_key : apikey.apikey_bomb, query: game_name, format: "jsonp", resources: "review"},
+                dataType: "jsonp",
+                success: function(data){
+                    GameData.tweets(data, genData.results, game_name);
+                }
+            });
+        });
+    },
+
+    gameDetails: function(game_id, game_name) {
         $(document).ready(function(){    
                 $.ajax({
                     url: "http://api.giantbomb.com/game/"+game_id+"/?json_callback=?",
@@ -18,13 +64,13 @@ var GameData = {
                     data: {api_key : apikey.apikey_bomb, format: "jsonp"},
                     dataType: "jsonp",
                     success: function(data){
-                        GameData.details(data, game_name);
+                        GameData.gameReview(data, game_name);
                     }
                 });
             });
     },
 
-   gamer: function(data) {
+    gamer: function(data) {
         $.get("/giantbomb/list.jade", function(template) {
             var html = jade.render(template, {
                 data: data
@@ -44,33 +90,6 @@ var GameData = {
                 success: function(data) { 
                     GameData.gamer(data);
                 } 
-            });
-        });
-    },
-
-    tweet: function(data) {
-
-      $.get("/giantbomb/twitterlist.jade", function(template) {
-          var html = jade.render(template, {
-              data: data
-          })
-        $("#list").html(html);
-      })
-       
-    },
-    
-    searchTweets: function(name) {
-        console.log("Making AJAX Call to /tweet_search (Replace with better way of referencing self)");
-        $(document).ready(function(){    
-            var test = $.ajax({
-                url: "http://localhost:8000/tweet_search", // our heroku address will go here until more clever way of referencing self is found ;) ;D
-                type: "get",
-                data: {
-                    q: name
-                },
-                dataType: "json", //was jsonp
-                success: function(data) { GameData.tweet(data) }, 
-                error: function(){ console.log("Error in Ajax Call (oh noes!)"); }
             });
         });
     },
