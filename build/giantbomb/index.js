f85e41a3342757cd9c776002f4406e55d54434e4
Andrea Sassu <andrea.sassu@colorado.edu>
2015-04-11 21:22:26 -0600

    merging in twitter functionality to single file, adding in better giant bomb ajax functionality

--- build/giantbomb/index.js
+++ build/giantbomb/index.js
@@ -1,10 +1,6 @@
 var GameData = {
 
-   details: function(data) {
-        for (var i = 0; i < data.results.genres.length; i++)
-        {
-            console.log(data.results.genres[i].name)
-        }
+   details: function(data, game_name) {
         $.get("/giantbomb/details.jade", function(template) {
             var html = jade.render(template, {
                 data: data.results
@@ -13,14 +9,17 @@ var GameData = {
         })
    },
 
-   gameDetails: function(game_id) {
-        console.log(game_id)
+   gameDetails: function(game_id, game_name) {
+        console.log(game_id);
         $(document).ready(function(){    
                 $.ajax({
-                    url: "http://api.giantbomb.com/game/"+game_id+"/",
+                    url: "http://api.giantbomb.com/game/"+game_id+"/?json_callback=?",
                     type: "get",
-                    data: {api_key : apikey.apikey_bomb, format: "jsonp", json_callback: "GameData.details"},
-                    dataType: "jsonp"
+                    data: {api_key : apikey.apikey_bomb, format: "jsonp"},
+                    dataType: "jsonp",
+                    success: function(data){
+                        GameData.details(data, game_name);
+                    }
                 });
             });
     },
@@ -38,10 +37,40 @@ var GameData = {
 
         $(document).ready(function(){    
             $.ajax({
-                url: "http://api.giantbomb.com/search/",
+                url: "http://api.giantbomb.com/search/?json_callback=?",
                 type: "get",
-                data: {api_key : apikey.apikey_bomb, query: name, format: "jsonp", resources: "game", json_callback: "GameData.gamer"},
-                dataType: "jsonp"
+                data: {api_key : apikey.apikey_bomb, query: name, format: "jsonp", resources: "game"},
+                dataType: "jsonp",
+                success: function(data) { 
+                    GameData.gamer(data);
+                } 
+            });
+        });
+    },
+
+    tweet: function(data) {
+
+      $.get("/giantbomb/twitterlist.jade", function(template) {
+          var html = jade.render(template, {
+              data: data
+          })
+        $("#list").html(html);
+      })
+       
+    },
+    
+    searchTweets: function(name) {
+        console.log("Making AJAX Call to /tweet_search (Replace with better way of referencing self)");
+        $(document).ready(function(){    
+            var test = $.ajax({
+                url: "http://localhost:8000/tweet_search", // our heroku address will go here until more clever way of referencing self is found ;) ;D
+                type: "get",
+                data: {
+                    q: name
+                },
+                dataType: "json", //was jsonp
+                success: function(data) { GameData.tweet(data) }, 
+                error: function(){ console.log("Error in Ajax Call (oh noes!)"); }
             });
         });
     },
